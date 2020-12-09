29059dd84b94508a125145c5b053aa6e610778ec
Jonathan Quesada <jonnythan.quesada@gmail.com>
2020-12-09 14:08:58 -0800

    inital commit

---
+++ __tests__/app.test.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const pool = require('../lib/utils/pool');
+
+describe('app test', () => {
+    it('verify sql is communicating with my server', () => {
+
+    });
+});
---
+++ index.js
@@ -0,0 +1,5 @@
+const app = require('./lib/app');
+
+app.listen(8456, () => {
+    console.log('listening on 8456');
+});
