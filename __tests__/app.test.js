2d1f24cd6b3438d3c7ae2d34756e02dfbf7c9d29
Jonathan Quesada <jonnythan.quesada@gmail.com>
2020-12-10 08:23:41 -0800

    add what I have

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,8 +1,32 @@
+const fs = require('fs');
 const request = require('supertest');
 const pool = require('../lib/utils/pool');
+const app = require('../lib/app');
+// const Phones = require('../lib/models/phones');
 
 describe('app test', () => {
-    it('verify sql is communicating with my server', () => {
+    beforeEach(() => {
+        return pool.query(fs.readFileSync('./data/setup.sql', 'utf-8'));
+    });
 
+    afterAll(() => {
+        return pool.end();
+    });
+
+    it('creates new phone via POST', async () => {
+        const response = await request(app)
+            .post('/api/v1/phones')
+            .send({
+                name: 'fds',
+                brand: 'ffs',
+                model: 'fdd'
+            });
+
+        expect(response.body).toEqual({
+            id: '1',
+            name: 'fds',
+            brand: 'ffs',
+            model: 'fdd'
+        });
     });
 });
